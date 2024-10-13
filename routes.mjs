4548f5d30703160e56db97a7baa10b8df8fe7fbc
LucasEspLau <lukaespinoza12@gmail.com>
2024-10-13 17:19:48 -0500

    se agrego login y registro de cliente

--- routes.mjs
+++ routes.mjs
@@ -1,5 +1,7 @@
 import express from 'express';
 import * as productController from './controllers/productController.mjs'
+import * as userController from './controllers/userController.mjs'
+
 import multer from 'multer';
 
 const upload = multer({ storage: multer.memoryStorage() });
@@ -10,5 +12,9 @@ router.get('/products', productController.getAllProducts)
 router.put('/products/:id',productController.updateProduct)
 router.post('/product-create',productController.createProduct)
 router.delete('/products/:id', productController.deleteProduct);
+
+router.post('/crear-cliente',userController.createUser)
+router.post('/login',userController.login)
+
 
 export default router;
